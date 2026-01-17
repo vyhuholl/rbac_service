9b6e6da318152628b60c773edca5fbac2c564741
Olga Pichuzhkina <vyhuholl@protonmail.com>
2026-01-17 05:10:13 +0300

    docs: create an OpenSpec change proposal for adding RBAC core functionality

--- rbac_service/urls.py
+++ rbac_service/urls.py
@@ -25,6 +25,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+    path(
+        'api/schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc',
+    ),
 ]
